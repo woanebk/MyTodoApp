e76c52817c4d73034fcfb2301941f9ab4b85c61b
namdd <123@123a>
2024-05-15 12:59:18 +0700

    fix bug render default group

--- src/screens/ListGroupScreen/index.tsx
+++ src/screens/ListGroupScreen/index.tsx
@@ -25,7 +25,7 @@ export default function ListGroupScreen({ }) {
     return (
       <View>
         <GroupCard icon={<Icon name='sunny' color={MyColors.orange} type='ionicons' />}
-          group={{ id: 'all', name: 'All', todos: groups.find(e => e.id === 'all')?.todos ?? [], mainColor: MyColors.orange }}
+          group={{ id: 'fun', name: 'All', todos: groups.find(e => e.id === 'fun')?.todos ?? [], mainColor: MyColors.orange }}
           isDefault={true}
         />
         <GroupCard icon={<Icon name='star-outline' color={MyColors.red} type='ionicons' />}
@@ -60,7 +60,7 @@ export default function ListGroupScreen({ }) {
       >
         {defaultGroups()}
         <View style={styles.divider} />
-        {groups?.map((item, index) =>
+        {groups?.filter(e => !['fun' , 'important', 'planned' , 'personal'].includes(e.id)).map((item, index) =>
           <GroupCard key={index.toString()} group={item} />
 
         )}
